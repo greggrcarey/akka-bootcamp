1b26b40ca6b07908de512268b2fdcaeec052a154
Gregg Carey <gregg.carey@outlook.com>
2025-04-23 10:23:38 -0400

    async and await, routers in akka net

--- unit-1/start/AkkaWordCounter2.App/Program.cs
+++ unit-1/start/AkkaWordCounter2.App/Program.cs
@@ -1,28 +1,22 @@
 ﻿using Akka.Hosting;
 using AkkaWordCounter2.App;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var hostBuilder = new HostBuilder();
 
 hostBuilder.ConfigureServices((context, services) =>
-{
-    services.AddAkka("MyActorSystem", (builder, sp) =>
     {
-        builder
-            .WithActors((system, registry, resolver) =>
+        services.AddHttpClient(); // needed for IHttpClientFactory
+
+        services.AddAkka("MyActorSystem", (builder, sp) =>
+        {
+            builder.ConfigureLoggers(logConfig =>
             {
-                var helloActor = system.ActorOf(Props.Create(() => new HelloActor()), "hello-actor");
-                registry.Register<HelloActor>(helloActor);
-            })
-            .WithActors((system, registry, resolver) =>
-            {
-                var timerActorProps =
-                    resolver.Props<TimerActor>(); // uses Msft.Ext.DI to inject reference to helloActor
-                var timerActor = system.ActorOf(timerActorProps, "timer-actor");
-                registry.Register<TimerActor>(timerActor);
+                logConfig.AddLoggerFactory();
             });
+        });
     });
-});
 
 var host = hostBuilder.Build();
 
